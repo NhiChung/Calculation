a6e57c9ef93603f24c4b1b34153c3cca309a756c
NhiChung <92297341+NhiChung@users.noreply.github.com>
2021-10-14 13:32:29 +0700

    Add files via upload

---
+++ Nhi_Chung_calculation/src/main/java/JavaStatsEx.java
@@ -0,0 +1,20 @@
+
+/**
+ * Starter class for MyStats application.
+ *
+ * @author janbodnar
+ */
+
+public class JavaStatsEx {
+
+    /**
+     * Application entry point.
+     *
+     * @param args application command line arguments
+     */
+    public static void main(String[] args) {
+
+        var app = new MyStatsApp();
+        app.run(args);
+    }
+}
