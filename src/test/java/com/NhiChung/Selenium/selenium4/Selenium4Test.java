9a943b4eacc2f959ed05d2b0fa850781f78f06bc
EPAM\Nhi_Chung <nhi_chung@epam.com>
2021-10-28 16:02:24 +0700

    follow E4 manual

--- src/test/java/com/NhiChung/Selenium/selenium4/Selenium4Test.java
+++ src/test/java/com/NhiChung/Selenium/selenium4/Selenium4Test.java
@@ -1,19 +1,15 @@
 package com.NhiChung.Selenium.selenium4;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
-import org.openqa.selenium.chrome.ChromeOptions;
+import com.NhiChung.Selenium.core.DriverWrapper;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Selenium4Test extends SeleniumBaseTest {
     @Test
     public void testingChromeAndSeleniumBasic() {
-        ChromeDriverService service = ChromeDriverService.createServiceWithConfig(new ChromeOptions());
-        WebDriver driver = new ChromeDriver(service);
-        driver.navigate().to("https://mvnrepository.com/");
-        driver.quit();
+        DriverWrapper.getDriver().navigate().to("https://mvnrepository.com/");
+        DriverWrapper.getDriver().quit();
         Assert.assertEquals(true, true);
     }
 }
