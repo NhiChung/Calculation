672693bc0826df9408cd2128673261efe53a30cf
Nibabao <nhilx150017@gmail.com>
2021-11-08 15:30:01 +0700

    create logback

--- src/test/java/com/NhiChung/Selenium/selenium4/Selenium4Test.java
+++ src/test/java/com/NhiChung/Selenium/selenium4/Selenium4Test.java
@@ -1,5 +1,6 @@
 package com.NhiChung.Selenium.selenium4;
 
+import com.NhiChung.Selenium.core.logger.MyLogger;
 import com.NhiChung.Selenium.google.pages.GoogleHomeSearchPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -12,17 +13,17 @@ public class Selenium4Test extends SeleniumBaseTest {
 
     @Test
     public void testingBasingSearchOnGoogleMainPage() {
-        log.info("start hello");
+        MyLogger.LOGGER.info("start hello");
         String title = new GoogleHomeSearchPage()
                 .setTextToSearchField("Vietnam")
                 .clickGoogleSearchButton().getTitle();
         Assert.assertTrue(title.contains("Vietnam"));
-        log.info("finish hello");
+        MyLogger.LOGGER.info("finish hello");
     }
 
     @Test
     void searchNormalText() {
-        log.info("Start verify Search normal test");
+        MyLogger.LOGGER.info("Start verify Search normal test");
 
         String title = new GoogleHomeSearchPage()
                 .setTextToSearchField("Vietnam")
@@ -30,37 +31,37 @@ public class Selenium4Test extends SeleniumBaseTest {
                 .getTitle();
         Assert.assertTrue(title.contains("Vietnam"));
 
-        log.info("Pass");
+        MyLogger.LOGGER.info("Pass");
     }
 
     @Test
     void trimText() {
         // Trim method() is the method returns a new string, without any of the leading or the trailing white spaces.
-        log.info("Start verify trim function of search field");
+        MyLogger.LOGGER.info("Start verify trim function of search field");
 
         String title = new GoogleHomeSearchPage()
                 .setTextToSearchField("        Vietnam       ")
                 .clickGoogleSearchButton().getTitle();
         Assert.assertTrue(title.contains("Vietnam"));
 
-        log.info("Pass");
+        MyLogger.LOGGER.info("Pass");
     }
 
     @Test
     void googleCalculator() {
-        log.info("Verify Google Calculator");
+        MyLogger.LOGGER.info("Verify Google Calculator");
         String result = new GoogleHomeSearchPage()
                 .setTextToSearchField("1+5")
                 .goToCalculatorPage()
                 .getCalculatorResult();
 
         Assert.assertTrue(result.equals("6"));
-        log.info("Pass");
+        MyLogger.LOGGER.info("Pass");
     }
 
     @Test
     void googleTranslate() {
-        log.info("Verify google Translate");
+        MyLogger.LOGGER.info("Verify google Translate");
 
         String result = new GoogleHomeSearchPage()
                 .setTextToSearchField("translate")
@@ -69,12 +70,12 @@ public class Selenium4Test extends SeleniumBaseTest {
 
         Assert.assertTrue(result.equals("hello")  || result.equals("Hello") || result.equals("Hi") || result.equals("Hi"));
 
-        log.info("Pass");
+        MyLogger.LOGGER.info("Pass");
     }
 
     @Test
     void googleTranslate2() {
-        log.info("Verify google Translate");
+        MyLogger.LOGGER.info("Verify google Translate");
 
         String result = new GoogleHomeSearchPage()
                 .setTextToSearchField("Vietnamese to English")
@@ -83,12 +84,12 @@ public class Selenium4Test extends SeleniumBaseTest {
 
         Assert.assertTrue(result.equals("hello")  || result.equals("Hello") || result.equals("Hi") || result.equals("Hi"));
 
-        log.info("Pass");
+        MyLogger.LOGGER.info("Pass");
     }
 
     @Test
     void colorPicker() {
-        log.info("Verify google Color Picker");
+        MyLogger.LOGGER.info("Verify google Color Picker");
 
         new GoogleHomeSearchPage()
                 .setTextToSearchField("color picker")
@@ -96,7 +97,7 @@ public class Selenium4Test extends SeleniumBaseTest {
                 .changeColorPicker("kyN4Jf3TGqF__slider-background");
 
         Assert.assertEquals(true, true);
-        log.info("Pass");
+        MyLogger.LOGGER.info("Pass");
 
     }
 }
